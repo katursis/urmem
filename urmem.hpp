38ad399af2862f2f74b6f385377be9f52cbd87b0
urShadow <videnex@gmail.com>
2017-11-01 12:16:01 +0000

    Delete get_call_address

--- urmem.hpp
+++ urmem.hpp
@@ -4,8 +4,6 @@
 #ifdef _WIN32
 #include <windows.h>
 #include <intrin.h> 
-
-#pragma intrinsic(_ReturnAddress) 
 #else
 #include <dlfcn.h>
 #include <sys/stat.h> 
@@ -30,19 +28,6 @@ public:
 		stdcall,
 		thiscall
 	};
-
-#ifdef _WIN32
-    __forceinline
-#else 
-    __attribute__((always_inline))
-#endif
-        static address_t get_call_address() {
-#ifdef _WIN32
-        return reinterpret_cast<address_t>(_ReturnAddress()) - 5;
-#else 
-        return reinterpret_cast<address_t>(__builtin_return_address(0)) - 5;
-#endif
-    }
 
 	template<calling_convention CConv, typename Ret = void, typename ... Args>
 	static Ret call_function(address_t address, Args ... args) {
